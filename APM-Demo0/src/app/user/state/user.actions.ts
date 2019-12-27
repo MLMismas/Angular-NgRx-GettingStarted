22cc9d3a32d04f8d4c867a20f2baa701e7cc35a6
MLMismas <52412840+MLMismas@users.noreply.github.com>
2019-12-27 15:58:36 -0500

    before effects

---
+++ APM-Demo0/src/app/user/state/user.actions.ts
@@ -0,0 +1,12 @@
+import { Action } from '@ngrx/store';
+
+export enum UserActionTypes {
+  MaskUserName = '[User] Toggle Mask User Name'
+}
+
+export class ToggleMaskUserName implements Action {
+  readonly type = UserActionTypes.MaskUserName;
+  constructor(public payload: boolean) { }
+}
+
+export type UserActions = ToggleMaskUserName;
